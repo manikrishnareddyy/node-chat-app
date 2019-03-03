fb893626b4abfe24d26a54f5b5250028612e311d
Mani Reddy <mani.lionel@gmail.com>
2019-03-03 22:16:58 +0530

    Init commit

---
+++ server/server.js
@@ -0,0 +1,11 @@
+const path= require('path');
+const express=require('express');
+const publicpath= path.join('__dirname','../public/');
+var app=express();
+
+app.use(express.static(publicpath));
+const port=process.env.PORT || 3000
+app.listen(port,()=>{
+    console.log("server is up on ${port}");
+});
+
