f9ba8e2e3dfe81c1cd4839bbd96eb75a5e863a2c
manikrishnareddyy <mani.lionel@gmail.com>
2019-03-04 22:27:32 +0530

    tset

--- server/server.js
+++ server/server.js
@@ -1,11 +1,38 @@
-const path= require('path');
-const express=require('express');
-const publicpath= path.join('__dirname','../public/');
-var app=express();
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketIO = require('socket.io');
 
-app.use(express.static(publicpath));
-const port=process.env.PORT || 3000
-app.listen(port,()=>{
-    console.log("server is up on ${port}");
-}); 
+const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
+var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
+app.use(express.static(publicPath));
+
+io.on('connection', (socket) => {
+  console.log('New user connected');
+
+  socket.emit('newMessage', {
+    from: 'Mani',
+    text: 'Hey. What is going on.',
+    createAt: 123
+  });
+
+  socket.on('createMessage',(message)=>{
+    console.log('createMessage', message);
+  });
+
+  socket.on('createEmail', (newEmail) => {
+    console.log('createEmail', newEmail);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User was disconnected');
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Server is up on ${port}`);
+});
